ccd5bbe4a732113a04c37bcbc7dfb1d1eb52c5d4
MiraDov290 <muzhyk.myroslava@gmail.com>
2024-02-01 13:22:13 +0000

    changers

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const List = styled.ul`
   display: grid;
-  max-width: calc(100vw - 48px);
+  /* max-width: calc(100vw - 48px); */
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: 16px;
   margin: 0 auto;
